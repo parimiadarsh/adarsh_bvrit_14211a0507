a6a82657a0c0ae6b3ba6a3360db6fc0cb7ff83fb
parimiadarsh <parimiadarsh@gmail.com>
2017-09-12 14:15:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.text.SimpleDateFormat;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		//Student s1 = new Student("14211a0507","Adarsh","1996/03/22","80");
+		String dates = "1999-03-17 00:00:00.0";
+		//SimpleDateFormat dt = new SimpleDateFormat(dates);
+	}
+
+}
